40494ac78ccaba07266cd94c5190b45b22923d48
L0rdGabbers <gabrielmarriage@me.com>
2023-12-15 15:44:20 +0000

    Removes REST_FRAMEWORK pagination, as it was preventing complete list of data from being retrieved in the React frontend.

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -33,7 +33,6 @@ REST_FRAMEWORK = {
         else 'dj_rest_auth.jwt_auth.JWTCookieAuthentication',
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10,
     'DATETIME_FORMAT': '%d %b %Y',
 }
 
