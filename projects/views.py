e333ee9299ea65b7a131b5a9fa581d80002258ea
L0rdGabbers <gabrielmarriage@me.com>
2023-12-17 17:37:46 +0000

    Undoes previous commit

--- projects/views.py
+++ projects/views.py
@@ -24,10 +24,7 @@ class ProjectList(generics.ListCreateAPIView):
         return queryset
 
     def perform_create(self, serializer):
-        collaborators_str = self.request.data.get('collaborators', '')
-        collaborators = [int(pk) for pk in collaborators_str.split(',') if pk]
-
-        serializer.save(owner=self.request.user, collaborators=collaborators)
+        serializer.save(owner=self.request.user)
 
     def post(self, request, *args, **kwargs):
         return self.create(request, *args, **kwargs)
