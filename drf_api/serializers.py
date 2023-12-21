20db78326bc4e7030c2a0d77580500a6956c6a32
L0rdGabbers <gabrielmarriage@me.com>
2023-12-21 14:05:00 +0000

    Adds is_owner to the CurrentUserSerializer

--- drf_api/serializers.py
+++ drf_api/serializers.py
@@ -5,8 +5,13 @@ from rest_framework import serializers
 class CurrentUserSerializer(UserDetailsSerializer):
     profile_id = serializers.ReadOnlyField(source='profile.id')
     profile_image = serializers.ReadOnlyField(source='profile.image.url')
+    is_owner = serializers.SerializerMethodField()
+
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
 
     class Meta(UserDetailsSerializer.Meta):
         fields = UserDetailsSerializer.Meta.fields + (
-            'profile_id', 'profile_image'
+            'profile_id', 'profile_image', 'is_owner',
         )
