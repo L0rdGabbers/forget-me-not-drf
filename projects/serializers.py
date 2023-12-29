089658a2641342d85afc3c2248b18a8487f29b3f
L0rdGabbers <gabrielmarriage@me.com>
2023-12-29 14:43:00 +0000

    does the same as previous commit, forgot to click save.

--- projects/serializers.py
+++ projects/serializers.py
@@ -104,7 +104,7 @@ class ProjectDetailSerializer(serializers.ModelSerializer):
 
     def get_completed_tasks(self, obj):
         tasks = Task.objects.filter(project=obj, complete=True)
-        task_data = [{'id': task.id, 'name': task.title} for task in tasks]
+        task_data = [{'id': task.id, 'name': task.title, 'is_owner': task.is_owner, 'is_collaborator': task.is_collaborator} for task in tasks]
         return task_data
 
     def get_uncompleted_tasks(self, obj):
